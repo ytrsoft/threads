c69d478bb8125820a66f5ceb71135c591301e4b2
ytrsoft <1654486219@qq.com>
2025-08-10 12:40:41 +0800

    ok

--- py/main.py
+++ py/main.py
@@ -1,6 +1,10 @@
 from sqlite import get_db, Post
+from sqlalchemy import func
 
 if __name__ == '__main__':
-    db = next(get_db())
-    posts = db.query(Post).all()
-    print(len(posts))
+  db = next(get_db())
+  posts = db.query(Post).order_by(func.random()).limit(10).all()
+  print(len(posts))
+  for post in posts:
+    print(post.id, post.title)
+
